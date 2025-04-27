07aed97b8db838f48d98c8cc400335c3c5f5444d
SanjaySargam <sargamsanjaykumar@gmail.com>
2025-04-27 17:21:26 +0530

    use cord

--- server.js
+++ server.js
@@ -3,11 +3,18 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const authRoutes = require('./routes/auth');
 const adminRoutes = require('./routes/admin');
+const cors=require("cors");
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(cors(corsOptions)) // Use this after the variable declaration
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
