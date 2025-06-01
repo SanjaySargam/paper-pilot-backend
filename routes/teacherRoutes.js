8db00823c4914b379425666ef3e02475b0e5f857
SanjaySargam <sargamsanjaykumar@gmail.com>
2025-06-01 12:45:53 +0530

    sdfs

--- routes/teacherRoutes.js
+++ routes/teacherRoutes.js
@@ -8,15 +8,15 @@ const checkRole = require('../middleware/checkRole');
 router.post('/createTeacher', auth, checkRole('admin'), teacherController.createTeacher);
 
 // Get all teachers for a school - admin only
-router.get('/', auth, checkRole('admin'), teacherController.getAllTeachers);
+router.get('/', auth, teacherController.getAllTeachers);
 
 // Get teacher by ID
 router.get('/:id', auth, teacherController.getTeacherById);
 
 // Update teacher - admin only
-router.put('/:id', auth, checkRole('admin'), teacherController.updateTeacher);
+router.put('/:id', auth, teacherController.updateTeacher);
 
 // Delete teacher - admin only
-router.delete('/:id', auth, checkRole('admin'), teacherController.deleteTeacher);
+router.delete('/:id', auth, teacherController.deleteTeacher);
 
 module.exports = router;
